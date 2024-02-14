105bcf268e8aafefc2ea7ca04618b3d13b0b9899
Zsombor Nyilas <zs.nyilas@yahoo.com>
2024-02-14 22:59:09 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Nyilas Zsombor on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
